7f8bbf321c76036bace3e39b71040f94761bbe09
Courtney Edwards <cedwarc@gmail.com>
2022-02-11 18:21:04 +0100

    Better timestamps

--- fftools/ffmpeg.c
+++ fftools/ffmpeg.c
@@ -4823,7 +4823,7 @@ static int transcode(void)
         // Log start_timestamp after recording starts
         if (transcode_iteration == transcode_steps_to_start_recording) {
             clock_gettime(CLOCK_REALTIME, &ts);
-            av_log(NULL, AV_LOG_INFO, "start_timestamp: %llu\n", 
+            av_log(NULL, AV_LOG_QUIET, "ffmpeg start_timestamp: %llu\n", 
                 llround((long long) ts.tv_sec * 1000 + ts.tv_nsec / 1e6));
             transcode_iteration = 0;
         }
--- fftools/ffplay.c
+++ fftools/ffplay.c
@@ -1375,7 +1375,7 @@ static void video_display(VideoState *is, int *start_timestamp_counter)
     SDL_RenderPresent(renderer);
     if ((*start_timestamp_counter) == steps_till_first_frame_is_displayed) {
         clock_gettime(CLOCK_REALTIME, &ts);
-        av_log(NULL, AV_LOG_QUIET, "start_timestamp: %llu\n", 
+        av_log(NULL, AV_LOG_QUIET, "ffplay start_timestamp: %llu\n", 
             llround((long long) ts.tv_sec * 1000 + ts.tv_nsec / 1e6));
         *start_timestamp_counter = 0;
     }
