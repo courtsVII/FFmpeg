011ba9d85a4c851f54d5266550fa3deb3c6397d5
Courtney Edwards <cedwarc@gmail.com>
2022-02-15 22:56:54 +0100

    Revert to 9a816177f305d42eea2c7f50d11535ab57efdc2b

--- fftools/ffmpeg.c
+++ fftools/ffmpeg.c
@@ -108,7 +108,6 @@
 
 const char program_name[] = "ffmpeg";
 const int program_birth_year = 2000;
-const int transcode_steps_to_start_recording = 3;
 
 static FILE *vstats_file;
 
@@ -4783,11 +4782,9 @@ static int transcode(void)
     AVFormatContext *os;
     OutputStream *ost;
     InputStream *ist;
-    struct timespec ts;
     int64_t timer_start;
     int64_t cur_time;
     int64_t total_packets_written = 0;
-    int transcode_iteration = 1;
 
     ret = transcode_init();
     if (ret < 0)
@@ -4819,18 +4816,6 @@ static int transcode(void)
         }
 
         ret = transcode_step();
-        
-        // Log start_timestamp after recording starts
-        if (transcode_iteration == transcode_steps_to_start_recording) {
-            clock_gettime(CLOCK_REALTIME, &ts);
-            av_log(NULL, AV_LOG_INFO, "start_timestamp: %llu\n", 
-                llround((long long) ts.tv_sec * 1000 + ts.tv_nsec / 1e6));
-            transcode_iteration = 0;
-        }
-        if (transcode_iteration) {
-            ++transcode_iteration;
-        }
-        
         if (ret < 0 && ret != AVERROR_EOF) {
             av_log(NULL, AV_LOG_ERROR, "Error while filtering: %s\n", av_err2str(ret));
             break;
