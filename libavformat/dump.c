df8f9a8b1196a5e6e3fad901d280c99c4b4ad602
Courtney Edwards <courtneyfe@google.com>
2025-07-30 10:25:41 +0100

    Port custom changes in libavformat/dump.c to latest master

--- libavformat/dump.c
+++ libavformat/dump.c
@@ -863,6 +863,7 @@ static void dump_stream_group(const AVFormatContext *ic, uint8_t *printed,
 void av_dump_format(AVFormatContext *ic, int index,
                     const char *url, int is_output)
 {
+    int i;
     struct timespec ts;
     uint8_t *printed = ic->nb_streams ? av_mallocz(ic->nb_streams) : NULL;
     if (ic->nb_streams && !printed)
