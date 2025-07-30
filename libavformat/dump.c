985dfb0a7143220a71bbad775fff4df6561bd976
Courtney Edwards <courtneyfe@google.com>
2025-07-30 13:36:38 +0100

    Update dump.c

--- libavformat/dump.c
+++ libavformat/dump.c
@@ -872,12 +872,13 @@ void av_dump_format(AVFormatContext *ic, int index,
 
     clock_gettime(CLOCK_REALTIME, &ts);
     av_log(NULL, AV_LOG_INFO, "%s #%d, %s, %s '%s' %s: %llu:\n",
-           is_output ? "Output" : "Input",
-           index,
-           is_output ? ic->oformat->name : ic->iformat->name,
-           is_output ? "to" : "from", url);
-           is_output ? "to" : "from", url, "start_timestamp", 
-           llround((long long) ts.tv_sec * 1000 + ts.tv_nsec / 1e6));
+       is_output ? "Output" : "Input",
+       index,
+       is_output ? ic->oformat->name : ic->iformat->name,
+       is_output ? "to" : "from",
+       url,
+       "start_timestamp",
+       llround((long long) ts.tv_sec * 1000 + ts.tv_nsec / 1e6));
     dump_metadata(NULL, ic->metadata, "  ", AV_LOG_INFO);
 
     if (!is_output) {
